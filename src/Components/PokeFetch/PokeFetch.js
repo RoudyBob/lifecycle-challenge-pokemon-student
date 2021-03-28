5ed9f4e4965a47bfb7664030adab14df5ae47c77
Bob Roudebush <bob@roudybob.net>
2021-03-28 16:01:27 -0400

    finally working

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,8 +9,29 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      secondsRemaining: 10,
+      showPokemon: false,
+      intervalId: '',
     }
   }
+
+  timer = () => {
+    var intervalId = setInterval(
+      () => this.tick(),
+      1000
+    );
+    this.setState({ intervalId: intervalId })
+  };
+
+  tick = () => {
+    console.log(this.state.secondsRemaining)
+    if (this.state.secondsRemaining > 0) {
+      this.setState({ secondsRemaining: (this.state.secondsRemaining - 1)})
+    } else {
+      clearInterval(this.state.intervalId)
+      this.setState( {showPokemon: true} )
+    }
+  };
 
   fetchPokemon() {
     let min = Math.ceil(1);
@@ -24,19 +45,27 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          secondsRemaining: 10,
+          showPokemon: false
         })
       })
       .catch((err) => console.log(err))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.pokeName !== prevState.pokeName ) {
+      this.timer();
+    }
   }
 
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 className={'timer'}>{this.state.secondsRemaining}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={'pokeImg'} style={this.state.showPokemon ? {filter: "brightness(100%)"} : {filter: "brightness(0%)"}} src={this.state.pokeSprite} alt="" />
+          <h1 className={'pokeName'} style={this.state.showPokemon ? {display: ""} : {display: "none"}}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
